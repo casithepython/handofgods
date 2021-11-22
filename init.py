63d497080f92f3def7c64a5d1c8f9d262dc9289d
casithepython <casithepython@gmail.com>
2021-11-22 00:04:33 -0600

    started designing init

--- init.py
+++ init.py
@@ -1,20 +1,23 @@
 import sqlite3
 import json
 
-connection = sqlite3.connect('AoPSCoin.db')
+connection = sqlite3.connect('HandOfGods.db')
 cursor = connection.cursor()
 
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
-amountOfCoins = 1000000
-coins = []
-for i in range(amountOfCoins):
-    coins.append(i)
-
-coins = json.dumps(coins)
-
-cursor.execute('INSERT INTO users (name,coins) VALUES (?,?)', ("AoPSCoin Central Bank", coins))
+attributes = [
+    "attack",
+    "defense",
+    "initiative",
+    "armor",
+    "research_cost_multiplier",
+    "research_rate_bonus",
+    "passive_population_growth_rate",
+    "income_per_functional",
+]
+cursor.execute('INSERT INTO users name VALUE ?', )
 
 connection.commit()
 connection.close()
