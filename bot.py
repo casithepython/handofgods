62d35a0e3baa0fb7c476ad6e435cae9bc630bb0d
DoubleThoughtTheProgrammer <DoubleThoughtTheProgrammer@gmail.com>
2021-11-24 13:52:44 +0800

    Put bot.py into testing mode

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 from discord.ext import commands
 import logging
 import SecretManager
+from asyncio import TimeoutError
 
-import main as db
+
+# import main as db
+import testdb as db
 
 debug_mode = True
 
@@ -22,10 +25,48 @@ async def research(ctx, *, tech_name):
     await ctx.send('You have not joined this game yet.')
     return
   else:
-    output_text = 'Attempt research of the technology "{}": react with :regional_indicator_a: for divine inspiration, '.format(tech_name)
-    await ctx.send(output_text)
+    # Phase 1: output text
+    output_text = 'Attempt research of the technology "{tech_name}":\n' \
+    ':regional_indicator_a: for divine inspiration (success probability {div_inspr_prob:.1%})\n'\
+    ':regional_indicator_b: for waking revalation (success probability {awake_rev_prob:.1%})\n'\
+    ':regional_indicator_c: for dream revalation (success probability {dream_rev_prob:.1%})\n'\
+    ':regional_indicator_d: to incarnate and command research (success probability {divine_avatar_prob:.1%})\n'\
+      .format(tech_name=tech_name, div_inspr_prob=db.get_attribute(player_id, db.Attributes.DIVINE_INSPIRATION_RATE),
+            awake_rev_prob=db.get_attribute(player_id, db.Attributes.AWAKE_REVELATION_RATE),
+            dream_rev_prob=db.get_attribute(player_id, db.Attributes.ASLEEP_REVELATION_RATE),
+            divine_avatar_prob=db.get_attribute(player_id, db.Attributes.DIVINE_AVATAR_RATE)
+            )
+    message = await ctx.send(output_text)
+    reactions = {
+      '\N{REGIONAL INDICATOR SYMBOL LETTER A}': 'divine_inspiration',
+      '\N{REGIONAL INDICATOR SYMBOL LETTER B}': 'awake_revelation',
+      '\N{REGIONAL INDICATOR SYMBOL LETTER C}': 'asleep_revelation',
+      '\N{REGIONAL INDICATOR SYMBOL LETTER D}': 'divine_avatar'
+    }
+    for reaction in reactions.keys():
+      await message.add_reaction(reaction)
+
+    def check(reaction, user):
+      if str(reaction.emoji) not in reactions:
+        return False
+      if user.id != ctx.author.id:
+        return False
+      if reaction.message.id != message.id:
+        return False
+      return True
     
-    pass
+    try:
+      user_reaction, _ = await bot.wait_for('reaction_add', timeout=30.0, check=check)
+      emoji = str(user_reaction.emoji)
+
+      # wait for reaction from correct player
+      for reaction in reactions.keys():
+        if reaction != emoji:
+          await message.remove_reaction(reaction, ctx.me)
+
+      
+    except TimeoutError:
+      ctx.send("Timed out")
     
   # ctx.author.id
 
@@ -33,9 +74,54 @@ async def research(ctx, *, tech_name):
 async def battle(ctx, player_name, soldier_count):
   pass
 
-@bot.command()
-async def test(ctx, echo_message):
-  await ctx.send('Echoing "{}"'.format(echo_message))
+# @bot.command()
+# async def test(ctx):
+#   tech_name = "Example Technology"
+#   output_text = 'Attempt research of the technology "{tech_name}":\n' \
+#     ':regional_indicator_a: for divine inspiration (success probability {div_inspr_prob:.1%})\n'\
+#     ':regional_indicator_b: for waking revalation (success probability {awake_rev_prob:.1%})\n'\
+#     ':regional_indicator_c: for dream revalation (success probability {dream_rev_prob:.1%})\n'\
+#     ':regional_indicator_d: to incarnate and command research (success probability {divine_avatar_prob:.1%})\n'\
+#       .format(tech_name=tech_name, div_inspr_prob=0.1,
+#             awake_rev_prob=0.2,
+#             dream_rev_prob=0.3,
+#             divine_avatar_prob=0.4123
+#   )
+#   message = await ctx.send(output_text)
+#   reactions = (
+#     '\N{REGIONAL INDICATOR SYMBOL LETTER A}',
+#     '\N{REGIONAL INDICATOR SYMBOL LETTER B}',
+#     '\N{REGIONAL INDICATOR SYMBOL LETTER C}',
+#     '\N{REGIONAL INDICATOR SYMBOL LETTER D}'
+#   )
+#   for reaction in reactions:
+#     await message.add_reaction(reaction)
+#   # await ctx.send('Echoing "{}"'.format(echo_message))
+#   message = await ctx.send(output_text)
+#   reactions = (
+#     '\N{REGIONAL INDICATOR SYMBOL LETTER A}',
+#     '\N{REGIONAL INDICATOR SYMBOL LETTER B}',
+#     '\N{REGIONAL INDICATOR SYMBOL LETTER C}',
+#     '\N{REGIONAL INDICATOR SYMBOL LETTER D}'
+#   )
+#   for reaction in reactions:
+#     await message.add_reaction(reaction)
+
+#   def check(reaction, user):
+#     if str(reaction.emoji) not in reactions:
+#       return False
+#     if user.id != ctx.author.id:
+#       return False
+#     if reaction.message.id != message.id:
+#       return False
+    
+
+#   user_reaction, _ = await bot.wait_for('reaction_add', timeout=30.0, check=check)
+
+#   # wait for reaction from correct player
+#   for reaction in reactions:
+#     if reaction != str(user_reaction.emoji):
+#       message.remove_reaction(reaction)
 
 
 def start_bot():
