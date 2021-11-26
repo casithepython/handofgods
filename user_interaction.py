cc5002ef5a6894dd99d08f83b62855d1915d138c
DoubleThoughtTheProgrammer <DoubleThoughtTheProgrammer@gmail.com>
2021-11-26 18:45:29 +0800

    Create a method that sends a message and waits for a user reaction

---
+++ user_interaction.py
@@ -0,0 +1,24 @@
+async def user_react_on_message(bot, ctx, text, wait_user, reactions):
+    message = await ctx.send(text)
+    for reaction in reactions.keys():
+        await message.add_reaction(reaction)
+
+    # Phase 2: Wait for user reaction
+    def check(reaction, user):
+        if str(reaction.emoji) not in reactions:
+            return False
+        if user.id != wait_user.id:
+            return False
+        if reaction.message.id != message.id:
+            return False
+        return True
+
+    try:
+        user_reaction, _ = await bot.wait_for('reaction_add', timeout=30.0, check=check)
+    except TimeoutError:
+        return None
+    emoji = str(user_reaction.emoji)
+    for reaction in reactions.keys():
+        if reaction != emoji:
+            await message.remove_reaction(reaction, ctx.me)
+    return reactions[emoji]
