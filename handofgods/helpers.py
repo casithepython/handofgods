b45d686b53af585739c1ebc6c010d0f638cb7a9f
SIGSTKFLT <sigstackfault@gmail.com>
2021-12-08 13:06:29 -0500

    document OneWord

--- handofgods/helpers.py
+++ handofgods/helpers.py
@@ -2,6 +2,8 @@ from discord.ext import commands
 
 
 class OneWord(commands.Converter):
+    """discord.py converter which ensures the
+    argument does not contain spaces"""
     async def convert(self, ctx, argument):
         if " " in argument:
             raise commands.BadArgument("must be one word")
